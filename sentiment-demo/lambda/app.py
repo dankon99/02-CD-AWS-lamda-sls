40cbc91aecb637d4ee9ff631330bcf7bb832cc57
dankon99 <daniel-kon@hotmail.com>
2023-08-24 12:48:49 +0000

    edited app

--- sentiment-demo/lambda/app.py
+++ sentiment-demo/lambda/app.py
@@ -2,17 +2,41 @@ import os
 import json
 import boto3
 
-def handler(event, context):
+#def handler(event, context):
+#
+ #   client = boto3.client('comprehend')
+#   body = event["body"]
+#   sentiment = client.detect_sentiment(LanguageCode = "en", Text = body)
+#   return {
+#           "statusCode": 200,
+#           "headers": {
+#               "Content-Type": "application/json"
+#           },
+#           "body": json.dumps({
+#                "sentiment ": json.dumps(sentiment)
+#            })
+#    }
+    
+from botocore.exceptions import BotoCoreError, ClientError
 
-    client = boto3.client('comprehend')
-    body = event["body"]
-    sentiment = client.detect_sentiment(LanguageCode = "en", Text = body)
-    return {
+def handler(event, context):
+    try:
+        client = boto3.client('comprehend')
+        body = event["body"]
+        sentiment = client.detect_sentiment(LanguageCode="en", Text=body)
+        return {
             "statusCode": 200,
             "headers": {
                 "Content-Type": "application/json"
             },
             "body": json.dumps({
-                "sentiment ": json.dumps(sentiment)
+                "sentiment": sentiment
             })
-    }
+        }
+    except (BotoCoreError, ClientError) as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "error": str(e)
+            })
+        }
